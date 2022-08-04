107cc322e264140988b4649fa940d226cbc7f7b5
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-03 21:18:54 -0300

    Se crea variable de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiSports: 'https://v3.football.api-sports.io',
 };
 
 /*
