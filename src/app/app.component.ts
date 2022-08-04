c8ba0a325bcdf5fa59eb0a78cb7ed0c7815acb42
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-03 21:17:36 -0300

    Testeo de endpoint api-sport. Se consume el servicio creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { WorldCupService } from './services/world-cup.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WorldCupService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'latinad-mundial';
+  info: any = [];
+  
+  constructor(private service: WorldCupService) {}
+
+  ngOnInit(): void {
+    this.service.getFixture('?league=1&season=2022').subscribe(
+      (info) => {
+        this.info = info.response.league;
+        console.log(info);
+      },
+      (e) => console.error(e)
+    );
+  }
 }
