d5c835665a82fd4e22637c3eb45c3febb0a525c1
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-11 03:23:15 -0300

    [ADD]Declaracion componente y exportacion

--- src/app/Components/resultados/resultados.module.ts
+++ src/app/Components/resultados/resultados.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResultadosComponent
+  ],
+  exports: [
+    ResultadosComponent
+  ],
   imports: [
     CommonModule
   ]
