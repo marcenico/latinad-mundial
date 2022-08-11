a2a87777c6af90f2050c7a91c7fd2706c7299a16
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-11 01:53:35 -0300

    [ADD]Modulo de marcador

---
+++ src/app/Components/marcador/marcador.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MarcadorComponent } from './marcador/marcador.component';
+
+@NgModule({
+    declarations: [
+        MarcadorComponent
+    ],
+    exports: [
+        MarcadorComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class MarcadorModule{}
