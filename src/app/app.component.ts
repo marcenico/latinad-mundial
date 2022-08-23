018a93775b41c73a8761f466d038a61ee5fd5523
Marcelo Puebla <marcenp23@gmail.com>
2022-08-22 21:17:58 -0300

    Agrego para pruebas con letra G se activa pantalla de gol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { swiperConfig1 } from './mocks/carousel-config.mocks';
 import { SwiperOptions } from 'swiper';
 
@@ -9,6 +9,16 @@ import { SwiperOptions } from 'swiper';
 })
 export class AppComponent {
   swiperConfig: SwiperOptions = swiperConfig1;
+  hayGol = false;
 
   constructor() {}
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (this.hayGol) return;
+    if (event.code == 'KeyG') {
+      this.hayGol = true;
+      setTimeout(() => (this.hayGol = false), 5000);
+    }
+  }
 }
