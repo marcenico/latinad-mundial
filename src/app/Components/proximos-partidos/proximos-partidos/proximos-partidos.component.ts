41fc5a81884de7518d25e90756cb4cbf146b6856
Marcelo Puebla <marcenp23@gmail.com>
2022-08-29 20:45:40 -0300

    Agrego para pedir los proximos partidos cada 6 horas

--- src/app/Components/proximos-partidos/proximos-partidos/proximos-partidos.component.ts
+++ src/app/Components/proximos-partidos/proximos-partidos/proximos-partidos.component.ts
@@ -3,6 +3,8 @@ import { Away, Home, Match } from 'src/app/models/live-matches.model';
 import { UtilsService } from 'src/app/services/utlis.service';
 import { WorldCupService } from 'src/app/services/world-cup.service';
 import { LiveMatches } from '../../../models/live-matches.model';
+import { interval } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-proximos-partidos',
@@ -15,13 +17,20 @@ export class ProximosPartidosComponent implements OnInit {
   constructor(private service: WorldCupService, private utilsService: UtilsService) {}
 
   ngOnInit(): void {
-    this.service.getMockProximosPartidos('?league=1&next=4').subscribe(
-      (res: LiveMatches) => {
-        res.response = res.response.slice(0, 4);
-        this.proximosPartidos = res.response;
-      },
-      (e) => console.error(e)
-    );
+    this.getProximosPartidos(0);
+    this.getProximosPartidos(6 * 1000 * 60 * 60); // 6 Horas
+  }
+
+  getProximosPartidos(delay: number) {
+    interval(delay)
+      .pipe(mergeMap(() => this.service.getMockProximosPartidos('?league=1&next=4')))
+      .subscribe(
+        (res: LiveMatches) => {
+          res.response = res.response.slice(0, 4);
+          this.proximosPartidos = res.response;
+        },
+        (e) => console.error(e)
+      );
   }
 
   getFlag(team: Home | Away) {
