eadf9e94f33fce2d7a633d7b00004e4445b528b9
Leonardo Puebla <leonardo38361@gmail.com>
2022-09-08 11:25:29 -0300

    imports de estilos

--- src/app/Components/goal/goal.component.ts
+++ src/app/Components/goal/goal.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.scss']
+  styleUrls: [
+    './goal.component.scss',
+    './goal-portrait.component.scss',
+    './goal-landscape.component.scss',
+  ],
 })
 export class GoalComponent implements OnInit {
   constructor() {}
